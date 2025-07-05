fd23023b3928613124513d7d9fcfabd022ca16ae
adelplanque <alaindelplanque@mailoo.org>
2025-07-06 01:09:48 +0200

    feat: Search for the build directory according to several possible names

--- flymake-meson-build.el
+++ flymake-meson-build.el
@@ -1,4 +1,4 @@
-;;; flymake-meson-build.el --- Flymake for meson build system -*- lexical-binding: t -*-
+;;; flymake-meson-build.el --- Flymake integration for Meson build system -*- lexical-binding: t -*-
 
 ;; Copyright (C) 2024 Alain Delplanque
 
@@ -28,10 +28,10 @@
 
 ;;; Commentary:
 
-;; Determines the compilation command to be used by flymake for a meson project.
-
-;; flymake-meson-build first finds the build command used by ninja to build the
-;; file, and then adapts it for use by flymake.
+;; This package integrates Flymake with the Meson build system.
+;; It extracts compilation commands from Ninja (Meson's backend)
+;; using `ninja -t commands` and sets `flymake-cc-command` accordingly,
+;; enabling accurate on-the-fly syntax checking.
 
 ;; Usage:
 
@@ -41,26 +41,44 @@
 
 ;; Configuration:
 
-;; By default flymake-meson-build considers that the project's build directory
-;; is "build".  To indicate another directory, you must modify the
-;; `flymake-meson-build-builddir' variable, for example by including a
-;; .dir-local.el file at the root of the project:
+;; `flymake-meson-build-builddirs` is a list of relative directory names
+;; where Meson's build output may reside, searched from the project root
+;; (the directory containing `meson.build`).
 
-;; ((c++-common . ((flymake-meson-build-builddir . "custom-builddir"))))
+;; By default, this list is '("build" "builddir").
+
+;; To customize globally, add in your init.el:
+;;
+;; (setq flymake-meson-build-builddirs '("build" "builddir" "custom-build"))
+
+;; To customize per-project, create a `.dir-locals.el` file at the project root:
+;;
+;; ((c++-mode . ((flymake-meson-build-builddirs . ("custom-build" "builddir")))))
+;; ((c-mode   . ((flymake-meson-build-builddirs . ("custom-build" "builddir")))))
 
 ;; Known limitations:
 
 ;; * Does not allow direct check of header files.
-;; * Has only been tested on c and c++ projects in a linux environment
+;; * Has only been tested on c and c++ projects in a Linux environment
 
 ;;; Code:
 
 (require 'flymake)
 
+(defgroup flymake-meson-build nil
+  "Flymake backend for the Meson build system."
+  :group 'flymake)
+
+(defcustom flymake-meson-build-builddirs '("build" "flymake-meson-build-builddir")
+  "List of possible Meson build directories relative to the project root."
+  :type '(repeat string)
+  :group 'flymake-meson-build)
+
 (defvar-local flymake-meson-build-topsrcdir nil
   "Root directory of the meson project.")
-(defvar-local flymake-meson-build-builddir "build"
-  "Relative path of meson builddir to the meson root project directory.")
+
+(defvar-local flymake-meson-build-builddir nil
+  "Build directory of the meson project.")
 
 (defconst flymake-meson-build--mode-language-alist
   '((c-mode . "c")
@@ -82,10 +100,23 @@
             (setq dir (file-name-directory (directory-file-name dir))))
           dir)))))
 
+(defun flymake-meson-build--get-builddir (topsrcdir)
+  "Search for the Meson build directory for the given TOPSRCDIR.
+
+Combine TOPSRCDIR with each entry in `flymake-meson-build-builddirs`,
+and return the first existing directory found.  Return nil if none exists."
+  (when (and topsrcdir
+             (listp flymake-meson-build-builddirs))
+    (seq-find
+     #'file-directory-p
+     (mapcar (lambda (dir)
+               (expand-file-name dir topsrcdir))
+             flymake-meson-build-builddirs))))
+
 (defun flymake-meson-build--get-ninja-command ()
   "Try to determine compile command to be used to compile current buffer file."
-  (let* ((builddir (concat flymake-meson-build-topsrcdir flymake-meson-build-builddir))
-         (filename (file-relative-name (buffer-file-name) builddir)))
+  (let* ((builddir flymake-meson-build-builddir)
+         (filename (file-relative-name (buffer-file-name) flymake-meson-build-builddir)))
     (with-temp-buffer
       (call-process "ninja" nil t nil "-C" builddir "-t" "commands")
       (goto-char (point-min))
@@ -96,10 +127,9 @@
   "Try to determine command to check syntax."
   (let ((cmd (flymake-meson-build--get-ninja-command)))
     (unless cmd (user-error "Cannot determine build command"))
-    (let* ((builddir (concat flymake-meson-build-topsrcdir flymake-meson-build-builddir))
-           (filename (file-relative-name (buffer-file-name) builddir))
+    (let* ((filename (file-relative-name (buffer-file-name) flymake-meson-build-builddir))
            (lang (alist-get major-mode flymake-meson-build--mode-language-alist))
-           (toks (append `("env" "-C" ,builddir)
+           (toks (append `("env" "-C" ,flymake-meson-build-builddir)
                          (seq-filter (lambda (x) (not (string= x filename)))
                                      (split-string cmd))
                          `("-fsyntax-only" "-x" ,lang "-fno-diagnostics-color" "-"))))
@@ -116,9 +146,12 @@ file.  Does nothing if the file does not appear to be part of a meson project."
   (with-current-buffer buf
     (unless flymake-meson-build-topsrcdir
       (setq flymake-meson-build-topsrcdir (flymake-meson-build--topsrcdir)))
-    (if flymake-meson-build-topsrcdir
-        (set (make-local-variable 'flymake-cc-command) #'flymake-meson-build--cc-command)
-      (message "Cannot determine root directory for a meson project"))))
+    (unless flymake-meson-build-builddir
+      (setq flymake-meson-build-builddir
+            (flymake-meson-build--get-builddir flymake-meson-build-topsrcdir)))
+    (if flymake-meson-build-builddir
+        (setq-local flymake-cc-command #'flymake-meson-build--cc-command)
+      (message "Unable to determine the build directory for the Meson project"))))
 
 (provide 'flymake-meson-build)
 ;;; flymake-meson-build.el ends here
